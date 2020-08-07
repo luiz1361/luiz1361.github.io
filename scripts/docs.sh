3d1087b7c19113a34f9493818fd1cd4009566d9e
Luiz Fernando Cotta Martins <luiz1361@gmail.com>
2020-08-08 00:10:34 +0100

    Fix

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,7 +2,7 @@
 
 for i in `find ../_docs -type f -name *.md -exec ls {} \;`; do
 cat $i | (echo "---" && cat ) | sponge $i
-cat $i | (echo $i | awk -F "/" '{ print "permalink: /docs/" $2 "/" $3 "/" }' && cat ) | sponge $i
-cat $i | (echo $i | awk -F "/" '{ print "title: " toupper($3) }' && cat ) | sponge $i
+cat $i | (echo $i | awk -F "/" '{ print "permalink: /docs/" $3 "/" $4 "/" }' && cat ) | sponge $i
+cat $i | (echo $i | awk -F "/" '{ print "title: " toupper($4) }' && cat ) | sponge $i
 cat $i | (echo "---" && cat ) | sponge $i
 done
